7c8b445c17fbf8e7a67cb39b60837bdd7e943aa9
Haza Nasrullah <hazanasrullah@gmail.com>
2024-07-07 15:37:06 +0700

    chore: Update page.jsx to use async/await syntax for importing LocomotiveScroll

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,21 +15,26 @@ export default function Home() {
 
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    async () => {
-      const LocomotiveScroll = (await import('locomotive-scroll')).default
-      const locomotiveScroll = new LocomotiveScroll();
-      setTimeout(() => {
-        setIsLoading(false);
-        document.body.style.cursor = 'default'
-        window.scrollTo(0, 0);
-      }, 2000)
-    }
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
 
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
   }, [])
 
   return (
-    <main >
+    <main className={styles.main}>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
         <Landing />
               <Description />
               <Projects />
